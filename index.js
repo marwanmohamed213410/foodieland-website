1e4be94e709be03f0cf6df5cb7701e72d80d070e
Marwan Mohamed <marwanmohamed2238@gmail.com>
2024-12-21 07:14:11 +0200

    finishing slider

--- index.js
+++ index.js
@@ -4,3 +4,31 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
+
+
+// slider control
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.home');
+let currentIndex = 0;
+
+function showSlide(index) {
+    slider.style.transform = `translateX(-${index * 100}%)`;
+    }
+    
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    }
+    
+    
+    setInterval(nextSlide, 5000);
+    
+    document.querySelector('.prev').addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        showSlide(currentIndex);
+    });
+    
+    
+    document.querySelector('.next').addEventListener('click', () => {
+        nextSlide();
+    });
