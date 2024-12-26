a3fb2c1de6e5cc5bf66e19279618f476d5659f27
Fahd74 <Fahdm644@gmail.com>
2024-12-27 01:01:13 +0200

    create css file for fav page & add navbar and footer in fav page

--- favorite page/favorite_script.js
+++ favorite page/favorite_script.js
@@ -60,21 +60,21 @@ function addToFavPage(itemDetails) {
             "M17.3631 11.0453C18.767 9.65157 21.0432 9.65157 22.4471 11.0453C23.851 12.439 23.851 14.6987 22.4471 16.0924L17 21.5L11.5529 16.0924C10.149 14.6987 10.149 12.439 11.5529 11.0453C12.9568 9.65157 15.233 9.65157 16.6369 11.0453L16.9996 11.4051L17.3631 11.0453ZM19.2426 4.75736C20.1831 5.69977 20.7461 6.87453 20.9318 8.09485C19.5993 7.84749 18.1932 8.08543 17.0001 8.808C14.8491 7.50516 12.0056 7.7777 10.1439 9.62594C8.01293 11.7414 7.95374 15.1405 9.96628 17.3273L10.1439 17.5117L12.565 19.916L10.9999 21.485L2.52138 12.993C0.417048 10.637 0.495706 7.01901 2.75736 4.75736C5.02157 2.49315 8.64519 2.41687 11.001 4.52853C13.35 2.42 16.98 2.49 19.2426 4.75736Z"
         );
         svgIcon.appendChild(path);
-        
-        
+        // Create anchor element for the Item
+        const itemLink= document.createElement('a');
+        itemLink.href = itemDetails.url;
+        itemDiv.appendChild(itemLink);
 
         // Add the image
-    
         const img = document.createElement('img');
         img.src = itemDetails.imageSrc;
         img.alt = itemDetails.title;
-        itemDiv.appendChild(img);
-    
+        itemLink.appendChild(img);
 
     // Add the title
     const title = document.createElement('h2');
     title.textContent = itemDetails.title;
-    itemDiv.appendChild(title);
+    itemLink.appendChild(title);
 
     // Add the Timer-restaurant div
     const timerDiv = document.createElement('div');
@@ -101,7 +101,7 @@ function addToFavPage(itemDetails) {
     timerIcon2.appendChild(timerIconElem2);
 
     const timerText2 = document.createElement('p');
-    timerText2.textContent = document.querySelector('p') || "Details"; // Default text if no span is available
+    timerText2.textContent = "Details"; // Default text if no span is available
 
     timerInnerDiv2.appendChild(timerIcon2);
     timerInnerDiv2.appendChild(timerText2);
