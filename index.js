47e1ba10fa7fd0c422618e71b18729d4e92e527f
Maryam Eid <160788995+marec1044@users.noreply.github.com>
2025-01-09 21:42:49 +0200

    update 1

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+
+// slider control
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.home');
+let currentIndex = 0;
+
+function showSlide(index) {
+    slider.style.transform = `translateX(-${index * 100}%)`;
+    }
+    
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    }
+    
+    
+    setInterval(nextSlide, 5000);
+    
+    document.querySelector('.prev').addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        showSlide(currentIndex);
+    });
+    
+    
+    document.querySelector('.next').addEventListener('click', () => {
+        nextSlide();
+    });
+    
+ 
