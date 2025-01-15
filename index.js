99ee01e1161e4e076dd40b839f40aa88ce8fb536
Fahd74 <Fahdm644@gmail.com>
2025-01-15 04:52:18 +0200

    the last touches in the project

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-}
+
 
 
 // slider control
